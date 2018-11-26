018a1d0a60fa64dd40b5978dc0460b9cb7032f1a
jp112sdl <jp112sdl@icloud.com>
2018-11-26 09:18:51 +0100

    add JS file

---
+++ Web_JS.h
@@ -0,0 +1,30 @@
+//
+// 2018-11-26 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
+//
+
+const char HTTP_JS[] PROGMEM = R"=====(
+<script>
+  function Get(u){ 
+    var h = new XMLHttpRequest(); 
+    h.open('GET',u,false); 
+    h.send(null); 
+    return h.responseText; 
+  } 
+
+  function refreshState(rekursiv) { 
+    var json_obj = JSON.parse(Get('/getValues')); 
+    document.getElementById('_uhrzeit').innerHTML = json_obj.uhrzeit;
+
+    var i;
+    for (i in json_obj.ziele) {
+     var element = document.getElementById('_ziel'+i);
+     if (element != null) element.innerHTML = json_obj.ziele[i];
+    }
+
+    if (rekursiv) setTimeout(function(){ refreshState(true); }, 1000); 
+  } 
+  
+  /*init refresh:*/ 
+  refreshState(true); 
+</script>
+)=====";
